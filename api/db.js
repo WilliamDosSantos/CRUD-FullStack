4722e971b43e973dd3501410ec27c5e83d79de56
charleslukes <charleslukes28@gmail.com>
2023-10-13 08:00:37 +0100

    use mysql2 and migration

--- api/db.js
+++ api/db.js
@@ -1,8 +1,23 @@
-import mysql from "mysql"
+import mysql from "mysql2";
+import migration from "mysql-migrations";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "willdev123",
-    database: "crud"
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "willdev123",
+  database: "crud",
+});
+
+db.getConnection(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+migration.init(db, __dirname + "/migrations", () => {
+  console.log("ran migration...");
+});
